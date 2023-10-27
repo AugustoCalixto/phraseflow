f20b750f5c72a6dc35354b96e6c48cb02f0f8b58
AugustoCalixto <gutodenovaes@outlook.com>
2023-10-27 20:39:29 -0300

    add url cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:62933',
+  });
   await app.listen(3000);
 }
 bootstrap();
